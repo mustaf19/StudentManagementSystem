0b0d7bbcb61e2ad1c482fed57eeb4267c87118e8
mustaf19 <mustafeez0001@gmail.com.com>
2026-07-17 23:37:34 +0530

    added to students object

--- objects/Students.java
+++ objects/Students.java
@@ -7,9 +7,69 @@ class Students{
     String bloodGroup;
     String dob;
 
-    Students(){
-
+    Students(String id, String name, String email, String address, String phoneNo, String bloodGroup, String dob){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNo = phoneNo;
+        this.bloodGroup = bloodGroup;
+        this.dob = dob;
     }
 
+    public String getId(){
+        return this.id;
+    }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public String getPhoneNo(){
+        return this.phoneNo;
+    }
+
+    public String getBloodGroup(){
+        return this.bloodGroup;
+    }
+
+    public String getDob(){
+        return this.dob;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public void setPhoneNo(String phoneNo){
+        this.phoneNo = phoneNo;
+    }
+
+    public void setBloodGroup(String bloodGroup){
+        this.bloodGroup = bloodGroup;
+    }
+
+    public void setDob(String dob){
+        this.dob = dob;
+    }       
 }

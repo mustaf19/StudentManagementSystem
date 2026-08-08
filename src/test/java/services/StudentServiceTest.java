9e2db8b517c689d2e47b3a479f0424d6c881768c
mustaf19 <mustafeez0001@gmail.com.com>
2026-08-08 23:16:27 +0530

    added mvn, unit test

---
+++ src/test/java/services/StudentServiceTest.java
@@ -0,0 +1,26 @@
+package services;
+
+import org.junit.jupiter.api.Test;
+import objects.Student;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @Test
+    void shouldNotAllowDuplicateStudentId() {
+
+        // Arrange
+        StudentService sf = new StudentService(new StudentFileService());
+        sf.addStudent(new Student("099", "John Doe", "john.doe@example.com", "123 Main St, Anytown, USA", "1234567890", "A+", "1990-01-01"));
+
+
+        // Act
+        boolean result = sf.addStudent(new Student("099", "John Doe 2", "john.doe2@example.com", "123 Main St, Anytown, USA", "1234567890", "A+", "1990-01-01"));
+
+
+
+        // Assert
+        assertFalse(result);
+
+    }
+}
